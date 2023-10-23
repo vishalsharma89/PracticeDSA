49e089372a5643914d9964efd48b985163a3ab3c
Asus <sharmavishal9643@gmail.com>
2023-10-23 15:56:53 +0530

    Tic Tac Toe And Stack And Queue

---
+++ src/OOPS1/StacksAndQueues/Stacks.java
@@ -0,0 +1,21 @@
+package OOPS1.StacksAndQueues;
+
+import java.util.Stack;
+
+public class Stacks {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(5);
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+    }
+}
